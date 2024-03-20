094ee5051a131b416ab4ae9a979e52e2bfd3a9bb
Loïc <lorandrianarivelo@gmail.com>
2024-03-20 22:37:05 +0300

    Ajout de ConfigJSF, création bean CustomerBean, configuration web.xmla et ajout de la page customerList.xhtml

---
+++ src/main/java/tp/customer/tpcustomerloic/config/ConfigJSF.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tp.customer.tpcustomerloic.config;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.faces.annotation.FacesConfig;
+
+/**
+ *
+ * @author Manoa Loic
+ */
+@ApplicationScoped
+@FacesConfig
+public class ConfigJSF {
+
+}
